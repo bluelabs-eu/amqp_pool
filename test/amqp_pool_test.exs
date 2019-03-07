598842a890ab08a333f28dd9a7fe7de8567e704b
Enrique Fernández <enrique@bluelabs.eu>
2019-03-07 12:05:14 +0100

    Add basic publish/consume test

--- test/amqp_pool_test.exs
+++ test/amqp_pool_test.exs
@@ -17,4 +17,29 @@ defmodule AMQPPoolTest do
 
     Enum.each(apps, fn app -> :ok = Application.stop(app) end)
   end
+
+  test "basic publish/consume test" do
+    {:ok, apps} = Application.ensure_all_started(:amqp_pool)
+
+    exchange_name = "hello"
+    exchange_type = :fanout
+    exchange_opts = []
+    queue_name = "hello-world"
+    queue_opts = []
+    rkey = "world"
+    payload1 = "Hello, world!"
+
+    {:ok, payload2, _metadata} =
+      AMQPPool.Channel.with_channel(fn channel ->
+        AMQP.Exchange.declare(channel, exchange_name, exchange_type, exchange_opts)
+        AMQP.Basic.publish(channel, exchange_name, rkey, payload1)
+        AMQP.Queue.declare(channel, queue_name, queue_opts)
+        AMQP.Queue.bind(channel, queue_name, exchange_name)
+        AMQP.Basic.get(channel, queue_name, no_ack: true)
+      end)
+
+    assert payload2 == payload1
+
+    Enum.each(apps, fn app -> :ok = Application.stop(app) end)
+  end
 end
