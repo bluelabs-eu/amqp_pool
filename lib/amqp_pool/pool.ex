cc2eb94affe50669c22911ee234e07d4731bb406
Enrique Fernández <enrique@bluelabs.eu>
2019-04-11 11:37:04 +0200

    Reduce code duplication

--- lib/amqp_pool/pool.ex
+++ lib/amqp_pool/pool.ex
@@ -55,39 +55,27 @@ defmodule AMQPPool.Pool do
     ]
   end
 
-  def connection_name(pool) do
-    env_var = env_var(pool, :connection_name)
-    param = param(pool, :connection_name)
-    fetch_value(env_var, param, & &1, :undefined)
+  def connection_name(pool), do: get_param(:connection_name, pool, & &1, :undefined)
+
+  def username(pool), do: fetch_param!(:username, pool)
+
+  def password(pool), do: fetch_param!(:password, pool)
+
+  def vhost(pool), do: get_param(:vhost, pool, & &1, "/")
+
+  def port(pool), do: get_param(:port, pool, &to_integer/1, 5672)
+
+  def host(pool), do: fetch_param!(:host, pool)
+
+  defp get_param(param_name, pool, transformation \\ & &1, default \\ nil) do
+    env_var = env_var(pool, param_name)
+    param = param(pool, param_name)
+    get_value(env_var, param, transformation, default)
   end
 
-  def username(pool) do
-    env_var = env_var(pool, :username)
-    param = param(pool, :username)
-    fetch_value!(env_var, param)
-  end
-
-  def password(pool) do
-    env_var = env_var(pool, :password)
-    param = param(pool, :password)
-    fetch_value!(env_var, param)
-  end
-
-  def vhost(pool) do
-    env_var = env_var(pool, :vhost)
-    param = param(pool, :vhost)
-    fetch_value(env_var, param, & &1, "/")
-  end
-
-  def port(pool) do
-    env_var = env_var(pool, :port)
-    param = param(pool, :port)
-    fetch_value(env_var, param, &to_integer(&1), 5672)
-  end
-
-  def host(pool) do
-    env_var = env_var(pool, :host)
-    param = param(pool, :host)
+  defp fetch_param!(param_name, pool, transformation \\ & &1) do
+    env_var = env_var(pool, param_name)
+    param = param(pool, param_name)
     fetch_value!(env_var, param)
   end
 
@@ -95,7 +83,7 @@ defmodule AMQPPool.Pool do
 
   def env_var(pool, param), do: [pool, param] |> Enum.join("_") |> String.upcase()
 
-  def fetch_value(env_var, param, transformation \\ & &1, default \\ nil) do
+  def get_value(env_var, param, transformation \\ & &1, default \\ nil) do
     value =
       (System.get_env(env_var) && env_var |> System.get_env() |> transformation.()) ||
         Application.get_env(:amqp_pool, param, default)
